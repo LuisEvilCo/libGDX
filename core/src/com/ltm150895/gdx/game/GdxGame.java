90e48f9163aad72bc1fb45728f36bdec8b0f8e1e
Luis Tinajero Muñozcano <ltm150895@gmail.com>
2015-11-29 16:08:57 -0600

    Add FPS count and obstacle.

--- core/src/com/ltm150895/gdx/game/GdxGame.java
+++ core/src/com/ltm150895/gdx/game/GdxGame.java
@@ -3,8 +3,11 @@ package com.ltm150895.gdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -19,19 +22,22 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 
 	private boolean DEBUG = false;
 	private float gravity_y = -9.8f;
-	private float gravity_x = 0f;
+	private float gravity_x = 0;
     private int horizontalforce = 0;
+    private SpriteBatch batch;
+    private BitmapFont font;
+    private float h , w;
 
 	private Box2DDebugRenderer b2dr;
 	private OrthographicCamera camera;
 	private World world;
-	public Body player, platform;
+	public Body player, platform, obstacule;
 
 	@Override
 	public void create () {
         Gdx.input.setInputProcessor(this);
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, w/2 , h/2);
@@ -39,8 +45,14 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 		world = new World(new Vector2(gravity_x, gravity_y), false);
 		b2dr = new Box2DDebugRenderer();
 
-		player = createBox(8, 10, 16, 16, false);
-		platform = createBox(0, 0, 64, 32, true);
+		player = createBox(-150, 10, 16, 16, false);
+        obstacule = createBox(0,-100 +32  , 50, 50 , true);
+		platform = createBox(0, - 100 , Gdx.graphics.getWidth() , 32, true);
+
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.CYAN);
+        font.getData().setScale(PPM/8);
 
 	}
 
@@ -50,7 +62,11 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 		Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		b2dr.render(world,camera.combined.scl(PPM));
+        batch.begin();
+        font.draw(batch, String.valueOf(Gdx.graphics.getFramesPerSecond()) + " h = " + String.valueOf(player.getPosition()) ,50, Gdx.graphics.getHeight() - 50  );
+        batch.end();
+
+		b2dr.render(world,camera.combined.scl(PPM*2));
 	}
 
 	@Override
@@ -61,6 +77,8 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 	public void dispose(){
 		world.dispose();
 		b2dr.dispose();
+        font.dispose();
+        batch.dispose();
 	}
 
 	private void update(float deltaTime) {
@@ -71,10 +89,10 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 
 	private void cameraUpdate(float deltaTime){
 		Vector3 position = camera.position;
-		/*position.x = player.getPosition().x * PPM ;
-		position.y = player.getPosition().y * PPM ;*/
-		position.x = platform.getPosition().x * PPM;
-        position.y = platform.getPosition().y * PPM;
+		position.x = player.getPosition().x * PPM ;
+		position.y = player.getPosition().y * PPM ;
+		/*position.x = platform.getPosition().x * PPM;
+        position.y = platform.getPosition().y * PPM;*/
 		camera.position.set(position);
 
 		camera.update();
@@ -122,7 +140,7 @@ public class GdxGame extends ApplicationAdapter implements InputProcessor{
 
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		player.applyForceToCenter(0, 50, false);
+		player.applyForceToCenter(50, 50, false);
         player.setLinearVelocity(horizontalforce * 5 , player.getLinearVelocity().y);
         return true;
     }
